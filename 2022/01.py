f9075510358a53f131e827f53473cd63cc55f301
douggyvegas <79470480+douggyvegas@users.noreply.github.com>
2022-12-01 12:45:37 +0100

    advent of code 2022 day 1

---
+++ 2022/01.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+import functools
+
+def main():
+    with open('01.in') as input:
+        sorted_cals = sorted(map(lambda group: sum([ int(cals) for cals in group.split('\n') if cals != "" ]), input.read().split('\n\n')), reverse=True)
+    
+    print(sorted_cals[0])
+    print(sum(sorted_cals[:3]))
+
+
+__name__ == "__main__" and main()
