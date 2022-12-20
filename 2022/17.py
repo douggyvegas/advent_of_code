7b5dd8b4648e80eb9c5393e8a3a070d2ab4b11f1
douggyvegas <79470480+douggyvegas@users.noreply.github.com>
2022-12-20 17:10:21 +0100

    WIP day 17 part 2

--- 2022/17.py
+++ 2022/17.py
@@ -11,7 +11,7 @@ def main():
     
     with open('17.in') as input:
         for line in input.read().splitlines():
-            gas_jets = cycle(line)
+            gas_jets = list(line)
 
     rocks = [ ] # horizontal slices (bitmasks) bottom to top
     rocks.append([ 15 << 2 ])
@@ -20,18 +20,65 @@ def main():
     rocks.append([ 1 << 5, 1 << 5, 1 << 5, 1 << 5 ])
     rocks.append([ 3 << 4, 3 << 4 ])
 
-    rocks = cycle(rocks)
-
     cave = [ 257 ] * 3
 
+    known_configurations = { }
+    current_gas_jet_index = 0
+    cycle = 0
+    base_target = 1E12
+    target = base_target
+    cycle_length = target
+    cycle_height = 0
+    highest = 0
     dropped_lines = 0
-    for count in range(1000000000000):
-        # print("==== STEP", count, "====")
-        # print(dropped_lines)
-        # for c in reversed(cave):
-        #     print(getSlice(c))
-        # print("")
+    while cycle < target:
+        if cycle % 100000 == 0:
+            print("==== STEP", cycle, "====")
+            for c in reversed(cave):
+                print(getSlice(c))
+            print("")
 
+            if cycle == 3:
+                exit()
+
+        current_rock_index = cycle % len(rocks)
+
+        if known_configurations is not None:
+            #print(known_configurations)
+            known_caves = known_configurations.get((current_rock_index, current_gas_jet_index), [])
+            if any([ cave in k for k in known_caves]):
+                print("Found an identical configuration!!!")
+                cycle_length = cycle
+                cycle_height = highest + dropped_lines
+                target = cycle - target % cycle
+                dropped_lines = 0
+                cycle = 0
+                known_configurations = None
+            else:
+                known_caves.append(cave)
+                known_configurations[(current_rock_index, current_gas_jet_index)] = known_caves
+                #print(known_caves)
+
+        # print(highest)
+        rock_y = highest + 4
+        next_rock = deepcopy(rocks[current_rock_index])
+        if len(cave) < rock_y + len(next_rock):
+            cave += [ 257 ] * (rock_y + len(next_rock) - len(cave))
+        while True:
+            gas_jet = gas_jets[current_gas_jet_index]
+            current_gas_jet_index = (current_gas_jet_index + 1) % len(gas_jets)
+            previous_rock = deepcopy(next_rock)
+            next_rock = [ s << 1 if gas_jet == "<" else s >> 1 for s in next_rock ]
+            # print("gj,r,nr =", gas_jet, [ getSlice(s) for s in previous_rock ], [ getSlice(s) for s in next_rock ])
+            if any([ s & cave[ rock_y + i ] for i, s in enumerate(next_rock) ]):
+                next_rock = previous_rock
+            rock_y -= 1
+            if rock_y == 0 or any([ s & cave[ rock_y + i ] for i, s in enumerate(next_rock) ]):
+                for i, s in enumerate(next_rock):
+                    cave[ rock_y + 1 + i ] |= s
+                break
+
+        # remove unecessary lines
         acc = 0
         lowest = 0
         for i in range(len(cave)):
@@ -44,28 +91,14 @@ def main():
             cave = cave[lowest::]
             dropped_lines += lowest
 
-        highest = max([ 0 ] + [ i for i in range(len(cave)) if cave[i] & 254 != 0 ])
+        highest = max([0] + [ i for i in range(len(cave)) if cave[i] & 254 != 0 ])
 
-        # print(highest)
-        rock_y = highest + 4
-        next_rock = deepcopy(next(rocks))
-        if len(cave) < rock_y + len(next_rock):
-            cave += [ 257 ] * (rock_y + len(next_rock) - len(cave))
-        while True:
-            gas_jet = next(gas_jets)
-            # print("gas_jet", gas_jet)
-            previous_rock = deepcopy(next_rock)
-            next_rock = [ s << 1 if gas_jet == "<" else s >> 1 for s in next_rock ]
-            # print("r,nr =", [ getSlice(s) for s in previous_rock ], [ getSlice(s) for s in next_rock ])
-            if any([ s & cave[ rock_y + i ] for i, s in enumerate(next_rock) ]):
-                next_rock = previous_rock
-            rock_y -= 1
-            if rock_y == 0 or any([ s & cave[ rock_y + i ] for i, s in enumerate(next_rock) ]):
-                for i, s in enumerate(next_rock):
-                    cave[ rock_y + 1 + i ] |= s
-                break
-            
-    highest = dropped_lines + max([ i for i in range(len(cave)) if cave[i] & 254 != 0 ])
+        cycle += 1
+
+
+    highest = max([ i for i in range(len(cave)) if cave[i] & 254 != 0 ])
+    print(base_target, cycle_length, cycle_height, highest, dropped_lines)
+    highest += dropped_lines + (base_target // cycle_length - 1) * cycle_height
     print(highest)
 
 
