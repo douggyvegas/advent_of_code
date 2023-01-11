55129bf9d9e791f9cba574c72b99e177533acb85
douggyvegas <79470480+douggyvegas@users.noreply.github.com>
2023-01-11 13:49:18 +0100

    day 22

--- 2022/22.py
+++ 2022/22.py
@@ -42,40 +42,40 @@ def main():
     part1 = False
     positions[tuple(position)] = facing
     instructions = re.split("([L|R])", lines[height + 1])
-    for instruction in instructions:
+    for i, instruction in enumerate(instructions):
         if instruction == 'L':
             facing = (facing - 1)  % 4
         elif instruction == 'R':
             facing = (facing + 1)  % 4
         else:
             steps = int(instruction)
-            direction = DIRECTIONS[facing]
             for _ in range(steps):
+                direction = DIRECTIONS[facing]
                 next_position = list(map(sum, zip(position, direction)))
                 next_facing = facing
                 if next_position[0] < 0 or next_position[1] < 0 or next_position[0] >= width or next_position[1] >= height or grid[next_position[0]][next_position[1]] == " ":
                     if facing == 0:
                         if part1: next_position[0] = 0
-                        elif next_position[1] < 50: next_position, next_facing = (99, 149 - next_position[1]), 2
-                        elif next_position[1] < 100: next_position, next_facing = (99 + next_position[1] % 50, 49), 3
-                        elif next_position[1] < 150: next_position, next_facing = (99, 49 - next_position[1] % 50), 2
-                        elif next_position[1] < 200: next_position, next_facing = (99 - next_position[1] % 50, 149), 3
+                        elif next_position[1] < 50: next_position, next_facing = (99, 149 - next_position[1]), 2 # a (2,0) -> (1,2)
+                        elif next_position[1] < 100: next_position, next_facing = (100 + next_position[1] % 50, 49), 3 # b (1,1) -> (2,0)
+                        elif next_position[1] < 150: next_position, next_facing = (149, 49 - next_position[1] % 50), 2 # a (1,2) -> (2,0)
+                        elif next_position[1] < 200: next_position, next_facing = (50 + next_position[1] % 50, 149), 3 # e (0,3) -> (1,2)
                     elif facing == 1:
                         if part1: next_position[1] = 0
-                        elif next_position[0] < 50: next_position, next_facing = (),
-                        elif next_position[0] < 100: next_position, next_facing = (),
-                        elif next_position[0] < 150: next_position, next_facing = (),
+                        elif next_position[0] < 50: next_position, next_facing = (100 + next_position[0], 0), 1 # c (0,3) -> (2,0)
+                        elif next_position[0] < 100: next_position, next_facing = (49, 150 + next_position[0] % 50), 2 # e (1,2) -> (0,3)
+                        elif next_position[0] < 150: next_position, next_facing = (99, 50 + next_position[0] % 50), 2 # b (2,0) -> (1,1)
                     elif facing == 2:
-                        if part1 == 2: next_position[0] = width - 1
-                        elif next_position[1] < 50: next_position, next_facing = (),
-                        elif next_position[1] < 100: next_position, next_facing = (),
-                        elif next_position[1] < 150: next_position, next_facing = (),
-                        elif next_position[1] < 200: next_position, next_facing = (),
+                        if part1: next_position[0] = width - 1
+                        elif next_position[1] < 50: next_position, next_facing = (0, 149 - next_position[1] % 50), 0 # d (1,0) -> (0,2)
+                        elif next_position[1] < 100: next_position, next_facing = (next_position[1] % 50, 100), 1 # f (1,1) -> (0,2)
+                        elif next_position[1] < 150: next_position, next_facing = (50, 49 - next_position[1] % 50), 0 # d (0,2) -> (1,0)
+                        elif next_position[1] < 200: next_position, next_facing = (50 + next_position[1] % 50, 0), 1 # g (0,3) -> (1,0)
                     elif facing == 3:
-                        if part1 == 3: next_position[1] = height - 1
-                        elif next_position[0] < 50: next_position, next_facing = (),
-                        elif next_position[0] < 100: next_position, next_facing = (),
-                        elif next_position[0] < 150: next_position, next_facing = (),
+                        if part1: next_position[1] = height - 1
+                        elif next_position[0] < 50: next_position, next_facing = (50, 50 + next_position[0] % 50), 0 # f (0,2) -> (1,1)
+                        elif next_position[0] < 100: next_position, next_facing = (0, 150 + next_position[0] % 50), 0 # g (1,0) - >(0,3)
+                        elif next_position[0] < 150: next_position, next_facing = (next_position[0] % 50, 199), 3 # c (2,0) -> (0,3)
                     
                     if part1:
                         while grid[next_position[0]][next_position[1]] == " ":
@@ -86,18 +86,21 @@ def main():
                     break
                 else:
                     position = next_position
-                    positions[tuple(position)] = next_facing
+                    facing = next_facing
+                    positions[tuple(position)] = facing
 
-            # os.system('clear')
-            # for y in range(height):
-            #     line = ""
-            #     for x in range(width):
-            #         f = positions.get((x, y), None)
-            #         if f == None:
-            #             line += grid[x][y]
-            #         else:
-            #             line += DIRECTIONS_CHARACTERS[f]
-            #     print(line)
+            if i % 100 == 0 or i == len(instructions) - 1:
+                os.system('clear')
+                print("    0" + " " * 49 + "50" + " " * 48 + "100" + " " * 47 + "150")
+                for y in range(height):
+                    line = "{0:03d}".format(y) + " "
+                    for x in range(width):
+                        f = positions.get((x, y), None)
+                        if f == None:
+                            line += grid[x][y]
+                        else:
+                            line += DIRECTIONS_CHARACTERS[f]
+                    print(line)
 
     print(position, facing, 1000 * (position[1] + 1) + 4 * (position[0] + 1) + facing)
 
